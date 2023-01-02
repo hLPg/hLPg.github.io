ad9273da511b7b1b070668fb0c70f30b5a85f227
hLPg <2749857721@qq.com>
2023-01-02 20:38:08 +0800

    Site updated: 2023-01-02 20:38:03

--- js/shouye.js
+++ js/shouye.js
@@ -1,15 +1,1 @@
-/* 首次访问弹窗 */
-$(function(){
-    if(window.localStorage.getItem("isClose") == 'yes'){
-        $('#note')[0].style.display='none';
-    }else
-    {
-         $('#note')[0].style.display='block';
-    }
-    
-});
-function closeclick()
-{
-   $('#note')[0].style.display='none';
-   window.localStorage.setItem("isClose", "yes");
-}
+function closeclick(){$("#note")[0].style.display="none",window.localStorage.setItem("isClose","yes")}$((function(){"yes"==window.localStorage.getItem("isClose")?$("#note")[0].style.display="none":$("#note")[0].style.display="block"}));
